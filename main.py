05dd0d8dab17a35b0829698dc874ba8b95ac00a3
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2022-11-18 09:47:17 +0200

    commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import requests
 
+def get_info_by_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        print(response)
+    except requests.exceptions.ConnectionError:
+        print('[!] Please check your connection!')
+
+def main():
+    ip = input('Please enter a target IP: ')
+    get_info_by_ip(ip=ip)
+
+if __name__ == '__main__':
+    main()
